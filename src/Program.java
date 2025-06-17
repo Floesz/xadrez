522cf718ec34106dd1c0431ab3a05d845e64867e
Guilherme Floes <guilhermefloesmmarchi@gmail.com>
2025-06-17 15:31:05 -0300

    Position

---
+++ src/Program.java
@@ -0,0 +1,9 @@
+import board.Position;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Position pos = new Position(5,3);
+        System.out.println(pos);
+    }
+}

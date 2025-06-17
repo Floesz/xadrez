807685d92a2d9bf390ddf72da1b17f212994b020
Guilherme Floes <guilhermefloesmmarchi@gmail.com>
2025-06-17 17:08:21 -0300

    Created ChessPiece, Color, Match and UI

--- src/Program.java
+++
@@ -1,9 +0,0 @@
-import board.Position;
-
-public class Program {
-    public static void main(String[] args) {
-
-        Position pos = new Position(5,3);
-        System.out.println(pos);
-    }
-}
---
+++ src/app/Program.java
@@ -0,0 +1,11 @@
+package app;
+
+import chess.Match;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Match match = new Match();
+        UI.printBoard(match.getPieces());
+    }
+}

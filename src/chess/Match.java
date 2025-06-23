c83cdcfe3ff35ad676ed508d93010c8a85752836
Guilherme Floes <guilhermefloesmmarchi@gmail.com>
2025-06-23 15:13:24 -0300

    ChessException

--- src/chess/Match.java
+++ src/chess/Match.java
@@ -25,8 +25,12 @@ public class Match {
         } return mat;
     }
 
+    private void placeNewPiece(char column,int row,ChessPiece piece){
+        board.placePiece(piece,new ChessPosition(column,row).toPosition());
+    }
     private void initialSetup(){
-        board.placePiece(new Hook(board,Color.WHITE),new Position(1,2));
-        board.placePiece(new King(board,Color.BLACK),new Position(4,0));
+        placeNewPiece('b',6,new Hook(board,Color.WHITE));
+        placeNewPiece('e',8,new King(board,Color.BLACK));
+        placeNewPiece('e',1, new King(board,Color.WHITE));
     }
 }

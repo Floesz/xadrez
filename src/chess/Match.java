2e1a806334d72524ee50a4120fdce5a1f413bfc8
Guilherme Floes <guilhermefloesmmarchi@gmail.com>
2025-06-25 11:00:59 -0300

    possible moves to Rook

--- src/chess/Match.java
+++ src/chess/Match.java
@@ -30,6 +30,7 @@ public class Match {
         Position source = sourcePosition.toPosition();
         Position target = targetPosition.toPosition();
         validateSourcePosition(source);
+        validateTargetPosition(source,target);
         Piece capturedPiece = makeMove(source,target);
         return (ChessPiece) capturedPiece;
     }
@@ -50,13 +51,28 @@ public class Match {
         }
     }
 
+    private void validateTargetPosition(Position source, Position target){
+        if(!board.piece(source).possibleMove(target)){
+            throw new ChessException("The chosen piece can't move to the target position!");
+        }
+    }
+
     private void placeNewPiece(char column,int row,ChessPiece piece){
         board.placePiece(piece,new ChessPosition(column,row).toPosition());
     }
     private void initialSetup(){
-        placeNewPiece('a', 1, new Rook(board, Color.WHITE));
-        placeNewPiece('d', 4, new King(board, Color.WHITE));
-        placeNewPiece('e', 8, new Rook(board, Color.BLACK));
-        placeNewPiece('g', 2, new King(board, Color.BLACK));
+        placeNewPiece('c', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 1, new King(board, Color.WHITE));
+        placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('c',7, new Rook(board,Color.BLACK));
+        placeNewPiece('c',8, new Rook(board,Color.BLACK));
+        placeNewPiece('d',7, new Rook(board,Color.BLACK));
+        placeNewPiece('d',8, new King(board,Color.BLACK));
+        placeNewPiece('e',7, new Rook(board,Color.BLACK));
+        placeNewPiece('e',8, new Rook(board,Color.BLACK));
+
     }
 }
